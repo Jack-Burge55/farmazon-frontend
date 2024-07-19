8afb8806e9a7d3a72d98d6557f21641c0d51c4d5
Jack Burgess <jack.burgess412@gmail.com>
2024-07-19 14:39:12 +0100

    updated footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
       <div className="content has-text-centered">
         <p>
           <strong>Farmazon</strong>, originally created by Parth, Chris, and Jack. Now updated and expanded by <a href="https://github.com/Jack-Burge55/">Jack</a>. View the Git Hub repo
-          <a href="https://github.com/Jack-Burge55/farmazon"> here</a>!</p>
+          <a href="https://github.com/Jack-Burge55/farmazon-backend"> here</a> (backend) and <a href="https://github.com/Jack-Burge55/farmazon-frontend">here</a> (frontend)!</p>
       </div>
     </footer>
   )
